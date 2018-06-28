a4e3483d57ea494ea34e5043ffb1adad2c7d5622
erikseyti <erikseyti@gmail.com>
2018-06-27 22:09:09 -0300

    calcular imc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  imc={};
+  listaImc=[];
 
   constructor(public navCtrl: NavController) {
 
+  }
+  calcularImc()
+  {
+    this.imc.resultado = parseFloat(this.imc.peso)/
+    ( Math.pow(parseFloat(this.imc.altura),2));
+    this.listaImc.push(this.imc);
+    this.imc={peso:'',altura:'',nome:''};
   }
 
 }
