6e655449014110c465bca036702ba4eb706f8b0e
erikseyti <erikseyti@gmail.com>
2018-06-27 22:28:31 -0300

    adicionado alert para exibir o imc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  imc={};
-  listaImc=[];
+  imc = {};
+  listaImc = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private alertCtrl:AlertController) {
   }
-  calcularImc()
-  {
-    this.imc.resultado = parseFloat(this.imc.peso)/
-    ( Math.pow(parseFloat(this.imc.altura),2));
+
+  calcularImc(){
+    this.imc.resultado = parseFloat(this.imc.peso)/Math.pow(parseFloat(this.imc.altura),2);
     this.listaImc.push(this.imc);
-    this.imc={peso:'',altura:'',nome:''};
+    this.imc = {peso:"", altura:"", nome:"", resultado:""};
   }
 
+  exibirIMC(imcLista){
+    this.alertCtrl.create({
+      title: 'Dados IMC!',
+      subTitle: 'Olá '+imcLista.nome+'seu imc é '+imcLista.resultado,
+      buttons: ['OK']
+    }).present();
+  }
 }
